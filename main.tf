13bf67f5a2de247b50d0ad143c373c870164af9d
RaviKiran8496 <ravikirang8496@gmail.com>
2024-08-11 07:36:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.62.0"
-    }
-  }
-}
+
 provider "aws" {
   region = "ap-south-1"  # Specify your preferred AWS region
 }
