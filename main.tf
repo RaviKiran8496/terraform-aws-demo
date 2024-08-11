3c7e600adef3e442e454c43de1be93a84d3e6f15
RaviKiran8496 <ravikirang8496@gmail.com>
2024-08-11 07:22:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.62.0"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-south-1"  # Specify your preferred AWS region
+}
+
+
 # Create a VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
